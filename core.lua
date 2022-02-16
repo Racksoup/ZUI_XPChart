f5fa38bed8e211e10dd0548624ff45024bfdd23d
Racksoup <rack.connor1@gmail.com>
2022-02-16 02:52:33 -0500

    grid complete

--- core.lua
+++ core.lua
@@ -16,7 +16,7 @@ SlashCmdList["XPC"] = function()
 end
 
 -- all xp to level 1-60 Classic WoW
-local XPToLevelClassic = {
+XPToLevelClassic = {
     400,    900,    1400,   2100,   2800,   3600,   4500,   5400,   6500,   7600, -- 1-10
     8800,   10100,  11400,  12900,  14400,  16000,  17700,  19400,  21300,  23200, -- 11- 20
     25200,  27300,  29400,  31700,  34000,  36400,  38900,  41400,  44300,  47400, -- 21-30
@@ -67,41 +67,41 @@ function XPC:CreateUI()
     frame:SetWidth(1200)
     frame:SetHeight(650)
     XPC:BuildChartLayout()
-    local line = frame:CreateLine()
-    line:SetColorTexture(0.7,0.7,0.7,.1)
-    line:SetStartPoint("TOPLEFT",10,10)
-    line:SetEndPoint("BOTTOMRIGHT",10,10)
     XPC_GUI.MainFrame:Hide()
 end
 
 function XPC:BuildChartLayout()
-    -- find and save highest amount of time played on any character, highest level of any character
     local mostTimePlayed = 0
     local highestLevel = 0
+    local XPOnLastLvl = 0
+    local XPOfHighestLevel = 0
+    local totalXPOfHighest = 0
+    -- find and save highest amount of time played on any character, highest level of any character
     for i,v in pairs(XPC.db.realm) do
         for j, k in ipairs(v) do
             if (k[1] > mostTimePlayed) then mostTimePlayed = k[1] end
-            if (k[2] > highestLevel) then highestLevel = k[2] end
+            if (k[2] > highestLevel) then highestLevel = k[2] XPOnLastLvl = k[3] end
         end
     end
+
+    -- save total amout of xp in highest lvl
+    for i = 1, highestLevel do 
+        XPOfHighestLevel = XPOfHighestLevel + XPToLevelClassic[i]
+    end
+    
+    -- save total amout of xp on highest xp character
+    totalXPOfHighest = XPOfHighestLevel + XPOnLastLvl
 
     local frameWidth = 1150
     local frameHeight = 600
     local frameWidthInterval = frameWidth / mostTimePlayed 
+    local frameHeightInterval = frameHeight / totalXPOfHighest
     local mostDaysPlayed = math.floor(XPC:StoD(mostTimePlayed))
     print(mostDaysPlayed)
     
 
-    XPC:BuildXAxis(mostTimePlayed, mostDaysPlayed, frameWidth, frameWidthInterval)
-    XPC:BuildYAxis()
-
-    local lastLevelPointText = XPC_GUI.MainFrame:CreateFontString(nil, "OVERLAY", "GameTooltipText")
-    lastLevelPointText:SetFont("Fonts\\FRIZQT__.TTF", 20, "THINOUTLINE")
-    lastLevelPointText:SetText(highestLevel)
-    lastLevelPointText:SetPoint("BOTTOMLEFT", 0, frameHeight)
-    
-
-
+    XPC:BuildXAxis(mostTimePlayed, mostDaysPlayed, frameWidthInterval, frameHeight)
+    XPC:BuildYAxis(highestLevel, frameHeightInterval, totalXPOHighest, XPOfHighestLevel, frameWidth)
 end
 
 function XPC:StoD(val)
@@ -112,7 +112,7 @@ function XPC:DtoS(val)
     return val * 60 * 60 * 24
 end
 
-function XPC:BuildXAxis(mostTimePlayed, mostDaysPlayed, frameWidth, frameWidthInterval)
+function XPC:BuildXAxis(mostTimePlayed, mostDaysPlayed, frameWidthInterval, frameHeight)
     -- find spacing. we want to divide by 5 then 4 then 3 then 2 trying to find a mod% full remainder value
     -- if mod == division
     -- else go with divide by 4 and decimal points
@@ -138,38 +138,47 @@ function XPC:BuildXAxis(mostTimePlayed, mostDaysPlayed, frameWidth, frameWidthIn
             fstring:SetFont("Fonts\\FRIZQT__.TTF", 20, "THINOUTLINE")
             fstring:SetText(mostDaysPlayed * (i / numOfTextObjs))
             fstring:SetPoint("BOTTOMLEFT", frameWidthInterval * XPC:DtoS(mostDaysPlayed) * (i / numOfTextObjs), 0)
+            local line = XPC_GUI.MainFrame:CreateLine()
+            line:SetColorTexture(0.7,0.7,0.7,.1)
+            line:SetStartPoint("BOTTOMLEFT", frameWidthInterval * XPC:DtoS(mostDaysPlayed) * (i / numOfTextObjs), 0)
+            line:SetEndPoint("TOPLEFT", frameWidthInterval * XPC:DtoS(mostDaysPlayed) * (i / numOfTextObjs), 0)
         end
     end
 end
 
-function XPC:BuildYAxis( )
+function XPC:BuildYAxis(highestLevel, frameHeightInterval, totalXPOfHighest, XPOfHighestLevel, frameWidth)
     -- find spacing. we want to divide by 5 then 4 then 3 then 2 trying to find a mod% full remainder value
     -- if mod == division
     -- else go with divide by 4 and decimal points
-    if (mostDaysPlayed > 5) then
+    if (highestLevel < 60) then
         local numOfTextObjs = 0
         local modNum = 0
 
-        -- mod mostDaysPlayed from 5 to 1.
-        for i=5, 0, -1 do      
-            modNum = mostDaysPlayed % i
+        -- mod highestLevel from 15 to 1.
+        for i=15, 0, -1 do      
+            modNum = highestLevel % i
             -- if modNum is 0 break the loop and set numOfTextObjs to i 
             if (modNum == 0) then
                 -- if we reach 1 numOfTextObjs should be 4
-                if (i == 1) then numOfTextObjs = 4 
+                if (i == 2) then numOfTextObjs = 4 
                 else numOfTextObjs = i end
                 break
             end 
         end
         
-        -- print x-axis text
+        -- print y-axis text
         for i=1, numOfTextObjs do 
             local fstring = XPC_GUI.MainFrame:CreateFontString(nil, "OVERLAY", "GameToolTipText")
             fstring:SetFont("Fonts\\FRIZQT__.TTF", 20, "THINOUTLINE")
-            fstring:SetText(mostDaysPlayed * (i / numOfTextObjs))
-            fstring:SetPoint("BOTTOMLEFT", frameWidthInterval * XPC:DtoS(mostDaysPlayed) * (i / numOfTextObjs), 0)
+            fstring:SetText(highestLevel * (i / numOfTextObjs))
+            fstring:SetPoint("BOTTOMLEFT", 0, frameHeightInterval * XPOfHighestLevel* (i / numOfTextObjs))
+            local line = XPC_GUI.MainFrame:CreateLine()
+            line:SetColorTexture(0.7,0.7,0.7,.1)
+            line:SetStartPoint("BOTTOMLEFT", 0, frameHeightInterval * XPOfHighestLevel* (i / numOfTextObjs))
+            line:SetEndPoint("BOTTOMRIGHT", 0, frameHeightInterval * XPOfHighestLevel* (i / numOfTextObjs))
         end
     end
 end
 
---make display widget
+--print lines from each axis-textobj
+--set data lines
